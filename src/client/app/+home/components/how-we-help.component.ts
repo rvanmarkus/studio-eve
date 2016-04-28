14a38216ee6227767a56d2a34ba9d883bf1905b9
Robbert van Markus <robbert@vanmarkus.com>
2016-04-28 20:30:22 +0200

    scroll

--- src/client/app/+home/components/how-we-help.component.ts
+++ src/client/app/+home/components/how-we-help.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from 'angular2/core';
 import { HowWeHelpService } from '../../shared/services/how-we-help.service';
 import { HelpMethod } from '../../shared/help-method.interface';
+import {PageScroll} from 'ng2-page-scroll';
+
 @Component({
 	selector: 'how-we-help',
 	viewProviders: [HowWeHelpService],
 	templateUrl: 'app/+home/components/how-we-help.component.html',
+	directives: [PageScroll],
 	styleUrls: ['app/+home/components/how-we-help.component.css']
 })
 export class HowWeHelpComponent implements OnInit {
