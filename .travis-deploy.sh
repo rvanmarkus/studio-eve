859cfbd3ec23ec6d934cf1eee5069f2e202871bb
Robbert van Markus <robbert@vanmarkus.com>
2016-04-28 23:07:33 +0200

    deploy

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -19,4 +19,4 @@ git commit -m "Deploy to GitHub Pages"
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 # tokens GH_TOKEN and GH_REF will be provided as Travis CI environment variables
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://9fe2bbeaf769f944d03df1f262fd8a5aa1df67b0@github.com/rvanmarkus/studio-eve" master:gh-pages > /dev/null 2>&1
