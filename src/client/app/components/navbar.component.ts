32ee2800c96c98a5ad182354c42e3b969ae09b9f
Robbert van Markus <robbert@vanmarkus.com>
2016-04-29 15:19:56 +0200

    navbar scroll to about!

--- src/client/app/components/navbar.component.ts
+++ src/client/app/components/navbar.component.ts
@@ -1,10 +1,11 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {PageScroll} from 'ng2-page-scroll';
 
 @Component({
   selector: 'sd-navbar',
   templateUrl: 'app/components/navbar.component.html',
   styleUrls: ['app/components/navbar.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, PageScroll]
 })
 export class NavbarComponent {}
