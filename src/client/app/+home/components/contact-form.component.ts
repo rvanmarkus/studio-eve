7415a0c626ce3a72894535e7d89c9f3be3d0f663
eefje-88 <eefje_auer@live.nl>
2017-05-31 17:57:25 +0200

    Update contact-form.component.ts

--- src/client/app/+home/components/contact-form.component.ts
+++ src/client/app/+home/components/contact-form.component.ts
@@ -27,7 +27,7 @@ export class ContactFormComponent {
 	}
 
 	sendMail() {
-		const formSpreeUrl = 'https://www.enformed.io/suv3b44g';
+		const formSpreeUrl = 'https://formspree.io/eefje_auer@live.nl';
 		const formDataJSON = JSON.stringify(this.contactForm.value);
 
 		let headers = new Headers();
